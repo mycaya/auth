5357ffea6765c7246df3658a737b856427077963
Micaiah Mackenzie <mmacke16@ford.com>
2019-10-20 18:25:32 -0400

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -32,6 +32,65 @@ module.exports = function(passport) {
    // FACEBOOK ================================================================
    // =========================================================================
 
+   passport.use(new FacebookStrategy({
+      clientID: '2424070964534047',
+      clientSecret: '2424070964534047',
+      callbackURL: 'https://figeur.com/auth/facebook/callback',
+      profileFields: ['name', 'email', 'link', 'locale', 'timezone', 'gender'],
+      passReqToCallback: true
+    }, (req, accessToken, refreshToken, profile, done) => {
+      if (req.user) {
+        User.findOne({ facebook: profile.id }, (err, existingUser) => {
+          if (err) { return done(err); }
+          if (existingUser) {
+            req.flash('errors', { msg: 'There is already a Facebook account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
+            done(err);
+          } else {
+            User.findById(req.user.id, (err, user) => {
+              if (err) { return done(err); }
+              user.facebook = profile.id;
+              user.tokens.push({ kind: 'facebook', accessToken });
+              user.profile.name = user.profile.name || `${profile.name.givenName} ${profile.name.familyName}`;
+              user.profile.gender = user.profile.gender || profile._json.gender;
+              user.profile.picture = user.profile.picture || `https://graph.facebook.com/${profile.id}/picture?type=large`;
+              user.save((err) => {
+                req.flash('info', { msg: 'Facebook account has been linked.' });
+                done(err, user);
+              });
+            });
+          }
+        });
+      } else {
+        User.findOne({ facebook: profile.id }, (err, existingUser) => {
+          if (err) { return done(err); }
+          if (existingUser) {
+            return done(null, existingUser);
+          }
+          User.findOne({ email: profile._json.email }, (err, existingEmailUser) => {
+            if (err) { return done(err); }
+            if (existingEmailUser) {
+              req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with Facebook manually from Account Settings.' });
+              done(err);
+            } else {
+              const user = new User();
+              user.email = profile._json.email;
+              user.facebook = profile.id;
+              user.tokens.push({ kind: 'facebook', accessToken });
+              user.profile.name = `${profile.name.givenName} ${profile.name.familyName}`;
+              user.profile.gender = profile._json.gender;
+              user.profile.picture = `https://graph.facebook.com/${profile.id}/picture?type=large`;
+              user.profile.location = (profile._json.location) ? profile._json.location.name : '';
+              user.save((err) => {
+                done(err, user);
+              });
+            }
+          });
+        });
+      }
+    }));
+    
+
+   /*
    //var fbStrategy = configAuth.facebookAuth;
    var fbStrategy = {};
    fbStrategy.clientID = '2424070964534047';
@@ -95,7 +154,7 @@ module.exports = function(passport) {
             });
          });
 
-      })); 
+      })); */
    // =========================================================================
    // GOOGLE ==================================================================
    // =========================================================================
